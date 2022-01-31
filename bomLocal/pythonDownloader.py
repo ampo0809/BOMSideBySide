f69c6c827a95b9eaee18af4449f22a5f5b831d6c
ampo0809 <ampuero0809@gmail.com>
2022-01-31 13:02:19 +0100

    Added local ukr

--- bomLocal/pythonDownloader.py
+++ bomLocal/pythonDownloader.py
@@ -17,7 +17,7 @@ booksAndChapters = [
     { "bookName": "Ether", "bookCode": "ether", "bookChapters": 15 },
     { "bookName": "Moroni", "bookCode": "moro", "bookChapters": 10 }]
 
-lang = "rus"
+lang = "ukr"
 
 for i in range(0, len(booksAndChapters)):
 
